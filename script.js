2dc5b9b4facfc9116268c4c5ab9f727eb9529f04
MudassirAlam7 <mudassiralam7@gmail.com>
2025-01-19 09:29:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 //your code here
+let clickedImage = []
 let imageContainer = document.querySelector('.flex')
+let reset = document.getElementById('reset')
+let verify = document.getElementById('verify')
+let para = document.getElementById('para')
 function generateImage() {
 	let images = ["img1", "img2","img3","img4","img5"]
 	let repeatedIndex = Math.floor(Math.random()*5)
@@ -16,7 +20,42 @@ function generateImage() {
 	})
 	
 }
-function handleImage() {
-	
+function handleImage(e) {
+	let clickedIndex = e.target.dataset.index
+	if(clickedImage.includes(clickedIndex)){
+		return
+	}
+	clickedImage.push(clickedIndex)
+	e.target.classList.add('selected')
+	reset.style.display = "block"
+
+	if(clickedImage.length ==2 ){
+		verify.style.display = "block"
+	}
 }
+reset.addEventListener('click', function (e) {
+	document.querySelectorAll('img').forEach(img=>{
+		img.classList.remove('selected')
+	})
+	clickedImage = []
+	reset.style.display = "none"
+	verify.style.display = "none"
+	para.innerText = ""
+})
+
+verify.addEventListener('click', function(){
+	if(clickedImage.length ===2){
+	let firstImg = imageContainer.children[clickedImage[0]].className
+	let secondImg = imageContainer.children[clickedImage[1]].className
+	
+	     if(firstImg == secondImg){
+		para.innerText = "You are a human. Congratulations!."
+	}
+		else{
+			para.innerText = "We can't verify you as a human. You selected the non-identical tiles."
+		}
+	}
+	clickedImage = []
+	verify.style.display = 'none'
+})
 	generateImage()
